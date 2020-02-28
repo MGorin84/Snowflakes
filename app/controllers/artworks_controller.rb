bca6340431122709e325d6d016d0c855cb154130
maria <gorinamaria@gmail.com>
2020-02-29 10:08:16 +1100

    fix errors

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,5 +1,6 @@
 class ArtworksController < ApplicationController
   before_action :set_artwork, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_owner, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   # GET /artworks
   # GET /artworks.json
